b0f1f2063e3ec6bf1ca9e5f216697bda660ba51f
KarlovS28 <sergokarlov2407@gmail.com>
2025-07-03 14:05:36 +0300

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,10 +5,10 @@ if (!process.env.DATABASE_URL) {
 }
 
 export default defineConfig({
-  out: "./migrations",
+  out: "./drizzle",
   schema: "./shared/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: "postgresql://dela:123456789@localhost:5432/dela",
   },
 });
